6920bf9fead3446d6b071afe47cbc59b5d8e0bbf
Jérome Freyre <jerome.freyre@gmail.com>
2013-10-14 16:16:28 +0200

    - first version of the demo project

--- DemoProject/DemoProject/JFEViewController.m
+++ DemoProject/DemoProject/JFEViewController.m
@@ -17,7 +17,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [[JFELocationManager sharedInstance] setDelegate:self];
+    
+    // Force to update label
+    [self didUpdateLocation:[[JFELocationManager sharedInstance] currentLocation]];
+    [self didUpdateHeading:[[JFELocationManager sharedInstance] currentHeading]];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +31,31 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Actions
+-(IBAction) changeDelegateType:(UISegmentedControl*)sender
+{
+    
+    if (sender.selectedSegmentIndex == 0) {
+        [[JFELocationManager sharedInstance] setDelegate:self];
+        [[JFELocationManager sharedInstance] setImportantDelegate:nil];
+    } else if (sender.selectedSegmentIndex == 1) {
+        [[JFELocationManager sharedInstance] setDelegate:nil];
+        [[JFELocationManager sharedInstance] setImportantDelegate:self];
+    }
+}
+
+
+#pragma mark - JFELocationManagerDelegate
+-(void)didUpdateHeading:(CLHeading *)newHeading
+{
+    double currentHeading = -newHeading.trueHeading * M_PI / 180;
+    [_headingLbl setText:[NSString stringWithFormat:@"%0.1f°", currentHeading]];
+}
+
+
+-(void) didUpdateLocation:(CLLocation *)location
+{
+    [_locationLbl setText:[NSString stringWithFormat:@"Lat: %.2f\nLon: %.2f", location.coordinate.latitude, location.coordinate.longitude]];
+    
+}
 @end
